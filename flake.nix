520276f5c3042f1d4acabc9f1aab614a46a70cf7
Jann Müller <j-mueller@noreply.users.github.com>
2026-08-31 14:46:12 +0200

    build: add Node.js TypeScript development flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Node.js and TypeScript development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_22
+              corepack
+              prettier
+              typescript
+              typescript-language-server
+            ];
+          };
+        }
+      );
+    };
+}
